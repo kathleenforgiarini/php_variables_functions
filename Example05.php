bdc80f42616d6c4a3b0f74a31f2e43495def6867
Kathleen Forgiarini da Silva <82175930+kathleenforgiarini@users.noreply.github.com>
2023-09-14 10:49:49 -0400

    using library file

---
+++ Example05.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ Subject: Using files
+ Authors: Kathleen Forgiarini
+ Date of creation: Sept 14, 2023
+ Dates of update:
+ This code shows you how to use files and libraries
+ 
+ */
+
+include 'library.php';
+
+$nb1 = 20;
+$nb2 = 30;
+$sum = 0;
+$mult = 0;
+
+calculate ($nb1, $nb2, $sum, $mult);
+
+echo "The sum of $nb1 and $nb2 is $sum <br/>";
+echo "The multiplication of $nb1 and $nb2 is $mult <br/>";
---
+++ library.php
@@ -0,0 +1,6 @@
+<?php
+
+function calculate ($nbr1, $nbr2, &$add, &$mul) {
+    $add = $nbr1 + $nbr2;
+    $mul = $nbr1 * $nbr2;
+}
